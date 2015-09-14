7bf33dfe5bea9df10443b516662f64c35b07ef63
Hunje Cho <akahun522@gmail.com>
2015-09-14 16:16:24 +0900

    Shows stderr and stdin pipes

--- git-up/main.go
+++ git-up/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 var targetRepository string
 
+const WHITE_SPACES string = " \t\n"
+
 func init() {
 	flag.StringVar(&targetRepository, "t", "", "target repository")
 	flag.Parse()
+}
+
+func getCurrentBranchName() string {
+	cmd := exec.Command("git", "get-branch-name")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return ""
+	}
+	return strings.TrimRight(out.String(), WHITE_SPACES)
 }
 
 func main() {
@@ -19,9 +34,23 @@ func main() {
 		return
 	}
 
-	cmd := exec.Command("git", "push", targetRepository, "`git get-branch-name`")
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("Error", err)
+	currentBranch := getCurrentBranchName()
+
+	if len(currentBranch) <= 0 {
+		fmt.Println("here is not under git")
+		return
 	}
+
+	fmt.Println("git", "push", targetRepository, currentBranch)
+	// cmd := exec.Command("git", "push", targetRepository, currentBranch)
+	var output bytes.Buffer
+	var errOut bytes.Buffer
+
+	cmd := exec.Command("git", "push", targetRepository, currentBranch)
+	cmd.Stdout = &output
+	cmd.Stderr = &errOut
+	cmd.Run()
+
+	fmt.Println(output.String())
+	fmt.Println(errOut.String())
 }
